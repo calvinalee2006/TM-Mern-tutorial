1a79b6977b85f5a94d62f4d9f5a0bad528b0844b
Calvin Lee <Anthony2006Lee@hotmail.com>
2023-08-08 07:01:06 -0500

    added user login registration, having w/ login

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -53,6 +53,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
+  //.remove() is depreciated
   await goal.deleteOne();
 
   res.status(200).json({ id: req.params.id });
